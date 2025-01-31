9fcc83b06ccc564492fdccc60b21021040dab561
Connor <107011324+connor33341@users.noreply.github.com>
2025-01-31 14:00:20 -0500

    update

--- static/js/serverStatus.js
+++ static/js/serverStatus.js
@@ -5,12 +5,14 @@ export async function GetJavaStatus(Url){
     let Connectable = false
     let Players = 0
     try {
+        console.log(JavaEndpoint+Url)
         const Response = await fetch(JavaEndpoint+Url)
         const Json = await Response.json()
+        console.log(Json)
         Connectable = Json["online"]
         Players = Json["players"]["online"]
     } catch (Error) {
-
+        console.warn(Error)
     }
     return Connectable,Players
 }
